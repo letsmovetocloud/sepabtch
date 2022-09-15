79a0108b26efef410568abd8eb97541d42312f7d
EC2 Default User <ec2-user@ip-172-31-40-176.ap-south-1.compute.internal>
2022-09-15 04:33:48 +0000

    second developer modifications done

--- ec2.tf
+++ ec2.tf
@@ -11,13 +11,13 @@ terraform {
 # Provider Block
 provider "aws" {
   profile = "default" 
-  region  = "us-east-1"
+  region  = "seconddeveloper-us-east-1"
 }
 
 #Resource Block
 resource "aws_instance" "ec2testserver" {
   ami           = "ami-041db4a969fe3eb68" 
-  instance_type = "t2.micro"
+  instance_type = "d4t.medium"
 
 }
 
